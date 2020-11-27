5a888e3d480c87b713624475f0387a0cce8c0af5
xiongyucheng <xiongyucheng@megvii.com>
2020-11-27 17:37:34 +0800

    :sparkles: add vim

---
+++ vim/chapter01/02-foo_bar.js
@@ -0,0 +1,18 @@
+var foo = 1
+var bar = 'a'
+var foobar = foo + bar
+
+-------在行尾添加分号-----
+
+var foo = 1;
+var bar = 'a';
+var foobar = foo + bar;
+
+组合键位
+A == $a 行尾添加
+I == ^a 行首插入
+o == A 下一行添加
+O == ko 上一行添加
+C == c$ 从光标到行尾改写
+s == cl 改写当前字符
+S == ^C 改写整行
---
+++ vim/chapter01/03-concat.js
@@ -0,0 +1,7 @@
+var foo = "method("argument1", "argument2")"
+------
+var foo = "method(" + argument1 + ", " + argument2 + ")"
+
+
+f{chart} 用于查找字符，将光标移动到指定位置
+; 可重复跳转光标
