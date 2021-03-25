f9f2573b8c43cdfaf1c1d6c5026d9842f9f4b9fc
xiongyucheng <xiongyucheng@megvii.com>
2021-03-25 11:11:17 +0800

    add

---
+++ node/02-chalk.js
@@ -0,0 +1,4 @@
+const chalk = require('chalk')
+
+console.log(chalk.yellow('hello'))
+console.log(chalk.blue('hello'))
---
+++ node/04-console.js
@@ -0,0 +1,10 @@
+console.log("--%d--", 123);
+console.log("--%s--", "dav");
+console.log("--%o--", Number);
+
+const x = 1;
+const y = 2;
+const z = 3;
+console.count("x 的值为 " + x + " 且已经检查了几次？");
+console.count("x 的值为 " + x + " 且已经检查了几次？");
+console.count("y 的值为 " + y + " 且已经检查了几次？");
